9f54b9af0ecf0475a29074e4ecfef8dfb338f662
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-03-17 14:35:15 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/DreamTeam">
+        <BrowserRouter basename="/fs-90-react-node-bogdanluckyman">
           <App />
         </BrowserRouter>
       </PersistGate>
